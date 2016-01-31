046c3832267c5c0c08d8a1a2e3cf17b1e7663f93
avaloni <avaloni@users.noreply.github.com>
2016-01-31 15:05:58 -0200

    colocando metodo __str__ para display dos models

--- epracas/atividades/models.py
+++ epracas/atividades/models.py
@@ -7,38 +7,65 @@ class Tipo(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome
+
 class Espaco(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
 
 class FaixasEtaria(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome
+
 class Parceiro(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
 
 class Publico(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome
+
 class Abrangencia(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
 
 class Periodicidade(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome
+
 class Area(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
 
 class Subarea(models.Model):
     area = models.ForeignKey(Area, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
 
 class Atividade(models.Model):
     nome = models.CharField(max_length=255)
@@ -57,3 +84,6 @@ class Atividade(models.Model):
     hora_inicio = models.TimeField()
     hora_termino = models.TimeField()
     publico_esperado = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
