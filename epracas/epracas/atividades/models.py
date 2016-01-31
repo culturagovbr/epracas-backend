3f14938d075749a3c1b3a8e957cea7f35293f6a9
Diego Aguilera <dorgam@gmail.com>
2016-01-31 15:54:12 -0200

    listas suspensas das atividades

--- epracas/epracas/atividades/models.py
+++ epracas/epracas/atividades/models.py
@@ -2,6 +2,35 @@ from django.db import models
 
 # Create your models here.
 
+class Tipos(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+class Espacos(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+class FaixasEtarias(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+class Parceiros(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+class Publico(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+class Abrangencia(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+class Periodicidade(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+
 class Areas(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
