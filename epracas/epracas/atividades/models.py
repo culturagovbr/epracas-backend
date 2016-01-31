65f9466a307ab0091e597c650cd9185e820e2061
Diego Aguilera <dorgam@gmail.com>
2016-01-31 15:48:19 -0200

    area e subarea
    modelos iniciais

--- epracas/epracas/atividades/models.py
+++ epracas/epracas/atividades/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Areas(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+class Subareas(models.Model):
+    area = models.ForeignKey(Areas, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
