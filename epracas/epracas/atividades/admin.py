b8079c3651eabba0230bda8b07086bc55f44d1e4
avaloni <avaloni@users.noreply.github.com>
2016-01-31 14:03:46 -0200

    registrando os models no admin

--- epracas/epracas/atividades/admin.py
+++ epracas/epracas/atividades/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+import models
+
 # Register your models here.
+admin.site.register(models.Tipos)
+admin.site.register(models.Espacos)
+admin.site.register(models.FaixasEtarias)
+admin.site.register(models.Parceiros)
+admin.site.register(models.Publico)
+admin.site.register(models.Abrangencia)
+admin.site.register(models.Periodicidade)
+admin.site.register(models.Areas)
+admin.site.register(models.Subareas)
+admin.site.register(models.Atividades)
