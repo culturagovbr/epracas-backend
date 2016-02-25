6a502af87629761fd0057b4503c8da2cb2baaaec
Gabriel Avaloni <avaloni@users.noreply.github.com>
2016-02-25 18:33:13 -0300

    correção pep8

--- epracas/atividades/views.py
+++ epracas/atividades/views.py
@@ -25,7 +25,12 @@ class ListarAtividade(ListView):
     def get_context_data(self, **kwargs):
         context = super(ListarAtividade, self).get_context_data(**kwargs)
         atividades_data = Atividade.objects.all().values('nome', 'data_inicio', 'data_termino')
-        atividades = list(map(lambda a: {'title': a['nome'], 'start': a['data_inicio'].strftime('%Y-%m-%d'), 'end': a['data_termino'].strftime('%Y-%m-%d')} , atividades_data))
+        atividades = list(map(lambda a:
+                              {
+                                  'title': a['nome'],
+                                  'start': a['data_inicio'].strftime('%Y-%m-%d'),
+                                  'end': a['data_termino'].strftime('%Y-%m-%d')
+                              }, atividades_data))
         context['atividades'] = json.dumps(atividades)
         return context
 
