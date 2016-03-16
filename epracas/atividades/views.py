459c98e3dc280e4a35e9b32713db23c81ea70b44
Gabriel Avaloni <avaloni@users.noreply.github.com>
2016-03-16 11:36:43 -0300

    incluindo a url para edição nos objetos de atividades

--- epracas/atividades/views.py
+++ epracas/atividades/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse_lazy, reverse
 from atividades.models import Atividade
 from atividades.forms import AtividadesForm
 from django.views.generic.list import ListView
@@ -16,6 +16,8 @@ class CriarAtividade(CreateView):
 
 class ModificarAtividade(UpdateView):
     model = Atividade
+    form_class = AtividadesForm
+    template_name = 'atividades/atividade_form.html'
     success_url = reverse_lazy('atividade-list')
 
 
@@ -24,12 +26,14 @@ class ListarAtividade(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ListarAtividade, self).get_context_data(**kwargs)
-        atividades_data = Atividade.objects.all().values('nome', 'data_inicio', 'data_termino')
+        atividades_data = Atividade.objects.all().values('id', 'nome', 'data_inicio', 'data_termino')
         atividades = list(map(lambda a:
                               {
                                   'title': a['nome'],
+                                  'allDay': 'false',
                                   'start': a['data_inicio'].strftime('%Y-%m-%d'),
-                                  'end': a['data_termino'].strftime('%Y-%m-%d')
+                                  'end': a['data_termino'].strftime('%Y-%m-%d'),
+                                  'url': reverse('atividade-update', args=[a['id']])
                                   }, atividades_data))
         context['atividades'] = json.dumps(atividades)
         return context
