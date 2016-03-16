c17009d1f0cf3aee3473b32bda7c5725e3b87cde
Gabriel Avaloni <avaloni@users.noreply.github.com>
2016-03-16 11:38:07 -0300

    inclui urls do smart_selects

--- epracas/epracas/urls.py
+++ epracas/epracas/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from atividades.views import CriarAtividade, ModificarAtividade, ListarAtividade, ExcluirAtividade
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'atividade/add/$', CriarAtividade.as_view(), name='atividade-add'),
     url(r'atividade/(?P<pk>[0-9]+)/$', ModificarAtividade.as_view(), name='atividade-update'),
-    url(r'$', ListarAtividade.as_view(), name='atividade-list'),
+    url(r'^$', ListarAtividade.as_view(), name='atividade-list'),
     url(r'^atividade/excluir/(?P<pk>[0-9]+)/$', ExcluirAtividade.as_view(), name='atividade-delete'),
+    url(r'^chaining/', include('smart_selects.urls')),
 ]
