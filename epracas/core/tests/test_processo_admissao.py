3e64eaecf62a1cb2b45fbbbede80e9fbfb2f3428
André  "decko" de Brito <decko@birosca.mobi>
2016-08-29 19:02:50 -0300

    Testa a persistencia de um processo de admissão usando POST. Testes falhando

--- epracas/core/tests/test_processo_admissao.py
+++ epracas/core/tests/test_processo_admissao.py
@@ -1,8 +1,12 @@
 import pytest
+import json
 
 from rest_framework import status
 from rest_framework.reverse import reverse
 
+from model_mommy import mommy
+
+from core.models import Gestor
 
 @pytest.mark.django_db
 def test_return_200_OK_to_Processo_list_URL(client):
@@ -16,3 +20,29 @@ def test_return_200_OK_to_Processo_list_URL(client):
             format='json'
     )
     assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_persist_a_process_using_POST(client):
+    """TODO: Docstring for test_list_all_process(client.
+    :returns: TODO
+
+    """
+
+    gestor = mommy.make(Gestor, nome = "Fulano Cicrano")
+
+    data = {
+            'gestor': gestor.id_pub
+    }
+
+    post = client.post(
+            reverse('core:processoadmissao-list'),
+            data,
+            format='json'
+    )
+
+    post_content = bytes.decode(post.content)
+    id_pub_gestor = json.loads(post_content).pop('gestor')
+
+    assert post.status_code == status.HTTP_201_CREATED
+    assert str(id_pub_gestor) in str(gestor.id_pub)
