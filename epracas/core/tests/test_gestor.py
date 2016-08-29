e6baecac9d1a04da5da5d983383eee3b9c692b74
André  "decko" de Brito <decko@birosca.mobi>
2016-08-28 23:04:08 -0300

    Testa a persistencia do nome do gestor. Teste falhando.

--- epracas/core/tests/test_gestor.py
+++ epracas/core/tests/test_gestor.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 
 from rest_framework import status
 from rest_framework.reverse import reverse
@@ -15,3 +16,24 @@ def test_return_200_ok_gestor_endpoint(client):
 
     response = client.get(reverse('core:gestor-list'))
     assert response.status_code == status.HTTP_200_OK
+
+@pytest.mark.django_db
+def test_persist_a_gestor_name(client):
+    """TODO: Docstring for test_return_a_gestor_list(client.
+    :returns: TODO
+
+    """
+
+    gestor = {
+            'nome': 'Fulano Cicrano'
+    }
+
+    response = client.post(
+            reverse('core:gestor-list'),
+            gestor,
+            format='json'
+    )
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert gestor['nome'] in bytes.decode(response.content)
+
