2b1b65f9f188eceaf2ac637e078be503e5dd4888
André  "decko" de Brito <decko@birosca.mobi>
2016-08-29 17:00:55 -0300

    Corrige o nome da view chamada pelo teste. Teste falhando.

--- epracas/core/tests/test_processo_admissao.py
+++ epracas/core/tests/test_processo_admissao.py
@@ -3,8 +3,8 @@ import pytest
 from rest_framework import status
 from rest_framework.reverse import reverse
 
-url_listing = reverse('core:processo-list')
 
+@pytest.mark.django_db
 def test_return_200_OK_to_Processo_list_URL(client):
     """TODO: Docstring for test_return_200_OK_to_Processo_list_URL(client.
     :returns: TODO
@@ -12,7 +12,7 @@ def test_return_200_OK_to_Processo_list_URL(client):
     """
 
     response = client.get(
-            url-listing,
+            reverse('core:processoadmissao-list'),
             format='json'
     )
     assert response.status_code == status.HTTP_200_OK
