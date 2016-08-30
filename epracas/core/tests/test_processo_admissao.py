d924966c95057fc1329b67791a6708d704e8f09d
André  "decko" de Brito <decko@birosca.mobi>
2016-08-30 15:55:41 -0300

    Testa o retorno de data de abertura de Processo de Admissão. Teste falhando

--- epracas/core/tests/test_processo_admissao.py
+++ epracas/core/tests/test_processo_admissao.py
@@ -6,7 +6,8 @@ from rest_framework.reverse import reverse
 
 from model_mommy import mommy
 
-from core.models import Gestor
+from core.models import Gestor, ProcessoAdmissao
+
 
 @pytest.mark.django_db
 def test_return_200_OK_to_Processo_list_URL(client):
@@ -46,3 +47,25 @@ def test_persist_a_process_using_POST(client):
 
     assert post.status_code == status.HTTP_201_CREATED
     assert str(id_pub_gestor) in str(gestor.id_pub)
+
+
+@pytest.mark.django_db
+def test_return_today_date_when_create_a_new_process(client):
+    """TODO: Docstring for test_return_today_date_when_create_a_new_process.
+    :returns: TODO
+
+    """
+    import pendulum
+    import datetime
+
+    gestor = mommy.make(Gestor, nome="Fulano Cicrano")
+    processo = mommy.make(ProcessoAdmissao, gestor=gestor)
+
+    data = processo.data_abertura.replace(tzinfo=None)
+
+    data_abertura = pendulum.instance(
+            data,
+            tz='America/Sao_Paulo'
+    )
+
+    assert data_abertura.is_today() == True
