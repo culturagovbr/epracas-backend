4e4b9be5e745cd93409052836523452f5b0409f6
André  "decko" de Brito <decko@birosca.mobi>
2017-03-08 18:07:24 -0300

    Testa o retorno do gestor atual de uma Praça. Testes falhando.

--- gestor/tests/test_gestor.py
+++ gestor/tests/test_gestor.py
@@ -1,11 +1,14 @@
 import pytest
 import json
 
+from django.contrib.auth import get_user_model
+
 from rest_framework import status
 from rest_framework.reverse import reverse
 
 from model_mommy import mommy
 
+User = get_user_model()
 pytestmark = pytest.mark.django_db
 
 
@@ -38,13 +41,31 @@ def test_return_a_praca_with_manager_information(client):
     Testa o retorno de uma Praça com a informação do seu atual gestor
     """
 
-    gestor = mommy.make('Gestor')
+    praca = mommy.make('Praca')
+    user = mommy.make(User, full_name="Fulano")
+    gestor = mommy.make('Gestor', praca=praca, user=user)
 
     response = client.get(reverse('pracas:praca-detail', kwargs={'pk':
-                                                                 gestor.praca.pk}))
+                                                                 praca.pk}))
 
     assert response.status_code == status.HTTP_200_OK
     assert 'gestor' in response.data
+    assert 'nome' in response.data['gestor']
+
+
+def test_return_only_the_current_praca_manager(client):
+    """
+    Testa o retorno apenas do atual gestor da Praça
+    """
+
+    praca = mommy.make('Praca')
+    gestor = mommy.make('Gestor', praca=praca, atual=True)
+    gestores = mommy.make('Gestor', praca=praca, _quantity=2)
+
+    response = client.get(reverse('pracas:praca-detail', kwargs={'pk':
+                                                                 praca.pk}))
+
+    assert response.status_code == status.HTTP_200_OK
     assert 'nome' in response.data['gestor']
 
 
