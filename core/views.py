fca01deeb77f7d61949e3a14332a356106ad6d68
André  "decko" de Brito <decko@birosca.mobi>
2017-03-08 23:24:46 -0300

    Altera identação nas views do Core.

--- core/views.py
+++ core/views.py
@@ -3,19 +3,13 @@ from rest_framework.viewsets import ModelViewSet
 
 
 class DefaultMixin(object):
-    filter_backends = (
-            filters.DjangoFilterBackend,
-            filters.SearchFilter,
-            )
+    filter_backends = (filters.DjangoFilterBackend, filters.SearchFilter)
 
 
 class MultiSerializerViewSet(ModelViewSet):
-
     def get_serializer_class(self):
         if self.action in self.serializers:
-            return self.serializers.get(
-                    self.action,
-                    self.serializers[self.action]
-                    )
+            return self.serializers.get(self.action,
+                                        self.serializers[self.action])
         else:
             return self.serializer_class
