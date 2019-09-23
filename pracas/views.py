f86768d82a8cb60da11f4cdc5e66990bdc603cb5
leonardo <volthier@gmail.com>
2019-09-23 16:06:59 -0300

    [UPDATE] Correção do falidador de retorno do status

--- pracas/views.py
+++ pracas/views.py
@@ -140,9 +140,9 @@ class ParceiroViewSet(DefaultMixin, ModelViewSet):
         parceiro = ParceiroDetailSerializer(data=request.data)
         if parceiro.is_valid():
             parceiro.save(praca=praca)
-            return Response(parceiro.data, status=201)
+            return Response(parceiro.data, status=status.HTTP_200_OK)
         else:
-            return Response(parceiro.errors, status=400)
+            return Response(parceiro.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def partial_update(self, request, pk=None, praca_pk=None):
         praca = get_object_or_404(Praca, pk=praca_pk)
