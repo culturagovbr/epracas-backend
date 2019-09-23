da0e43f5fc4080eeee4d1ac334b8786c3a27e1fe
leonardo <volthier@gmail.com>
2019-09-23 17:30:05 -0300

    [UPDATE] Correção da serializer

--- pracas/serializers.py
+++ pracas/serializers.py
@@ -129,7 +129,7 @@ class ParceiroBaseSerializer(serializers.ModelSerializer):
 
 class ParceiroDetailSerializer(ParceiroBaseSerializer):
     class Meta:
-        model = Parceiro
+        model = Parceiro(allow_null=True)
         fields = ('id_pub', 'praca', 'nome', 'endereco', 'contato', 'telefone', 'email',
                   'ramo_atividade', 'acoes', 'tempo_parceria', 'recursos_financeiros', 'imagem')
 
