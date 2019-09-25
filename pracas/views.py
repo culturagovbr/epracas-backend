81a9b65d7ce93dfd95b2521ecd0f0ae0a333fe47
leonardo <volthier@gmail.com>
2019-09-25 16:52:20 -0300

    [FIX] Correção Final implementada apenas no request-data

--- pracas/views.py
+++ pracas/views.py
@@ -147,10 +147,12 @@ class ParceiroViewSet(DefaultMixin, ModelViewSet):
     def partial_update(self, request, pk=None, praca_pk=None):
         praca = get_object_or_404(Praca, pk=praca_pk)
         parceiro = get_object_or_404(Parceiro, pk=pk)
-
         self.check_object_permissions(request, praca)
+        if request.data['tempo_parceria'] == 'null':
+            request.data._mutable = True
+            request.data['tempo_parceria'] = None
         serializer = ParceiroDetailSerializer(parceiro, data=request.data,
-                                              partial=True, allow_null=True)
+                                              partial=True)
         if serializer.is_valid():
             serializer.save(Parceiro=parceiro)
             return Response(serializer.data, status=status.HTTP_200_OK)
